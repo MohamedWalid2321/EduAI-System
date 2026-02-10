09872f38ac374fedbd8fbbdc35f3f1463b0758e7
Mohaned <mohanedmohamed267@gmail.com>
2026-02-10 20:40:30 +0200

    Intitial_Create

--- Backend/Edu_Ai_API_Solution/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Backend/Edu_Ai_API_Solution/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35a995aad60ff0b3012fa09342287be3a128b900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96c05c9fda2129c80c510586f67416bfa7fb9a66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
