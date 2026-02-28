2f34f0343f5a72e381611df3abb10f00c2d55802
Mohaned <mohanedmohamed267@gmail.com>
2026-02-28 07:30:27 +0200

    Configure Jwt Authuntication  in swagger and solving Account Problem

--- Backend/Edu_Ai_API_Solution/Core/ServiceLayer/Mapping/MappingConfiguration.cs
+++ Backend/Edu_Ai_API_Solution/Core/ServiceLayer/Mapping/MappingConfiguration.cs
@@ -35,6 +35,17 @@ namespace ServiceLayer.Mapping
 			config.NewConfig<RegisterRequest, ApplicationUser>()
 			.Map(dest => dest.UserName, src => src.Email);
 
+			config.NewConfig<(ApplicationUser user, IList<string> roles), UserResponse>()
+			.Map(dest => dest, src => src.user)
+			.Map(dest => dest.Roles, src => src.roles);
+			config.NewConfig<CreateUserRequest, ApplicationUser>()
+			.Map(dest => dest.UserName, src => src.Email)
+			.Map(dest => dest.EmailConfirmed, src => true);
+
+			config.NewConfig<UpdateUserRequest, ApplicationUser>()
+				.Map(dest => dest.UserName, src => src.Email)
+				.Map(dest => dest.NormalizedUserName, src => src.Email.ToUpper());
+
 		}
 	}
 }
